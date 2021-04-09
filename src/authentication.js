5fff7a61e3b13d9ec4a121d2fac5464bc46330b4
Joey Chen <jay.hatelove@gmail.com>
2021-04-09 14:06:23 +0800

    [Otp]: import Module function (non-class style)

--- src/authentication.js
+++ src/authentication.js
@@ -1,11 +1,11 @@
 import {Profile} from "./profile";
-import {Otp} from "./otp";
+import * as otp from "./otp";
 
 export class Authentication {
     is_valid(account, password) {
         const profile = new Profile();
         const password_from_profile = profile.get_password(account);
-        const otp = new Otp();
+        // const otp = new Otp();
         const token = otp.get_token();
         console.log(`password:${password_from_profile}, token:${token}`);
 
--- src/otp.js
+++ src/otp.js
@@ -1,5 +1,8 @@
-export class Otp {
-    get_token() {
-        return Math.floor(Math.random() * Math.floor(1000000));
-    }
+// export class Otp {
+function get_token() {
+    return Math.floor(Math.random() * Math.floor(1000000));
 }
+
+export {get_token};
+
+// }
